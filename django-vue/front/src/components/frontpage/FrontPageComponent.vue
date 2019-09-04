d6cc0cdac93e53d6c6a6380cc95cf946f2d86654
ronaldo3r <ronaldo3-0827@hotmail.com>
2019-09-04 16:13:29 -0500

    add frontpage

--- django-vue/front/src/components/frontpage/FrontPageComponent.vue
+++ django-vue/front/src/components/frontpage/FrontPageComponent.vue
@@ -1,4 +1,49 @@
 <template>
+<div>
+
+<v-row>
+
+
+<v-col>
+
+
+<h1>
+{{title}}
+
+</h1>
+
+<p>
+{{text}}
+</p>
+
+<v-btn
+:ripple="false"
+ class="ma-2 next"
+outlined
+ color="#673ab7"
+@click="e1 = 2"
+>
+
+{{buttonText}}
+
+</v-btn>
+
+</v-col>
+
+
+<v-col>
+
+
+
+
+
+</v-col>
+
+</v-row>
+
+
+
+</div>
 
 
 
@@ -9,7 +54,23 @@ export default {
   name: "FrontPage",
   data() {
     return {
+
+      title:"Narrativas de Paz",
+      text:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam egestas, metus ac dignissim mattis, metus risus pretium ligula, ut mattis purus eros at nisl. ",
+      buttonText:"Quiero construir Paz"
         };
   }
 };
 </script>
+
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Poppins|Roboto&display=swap");
+body {
+  background-color: white;
+}
+
+h1{
+  text-color:#0C186D;
+}
+</style>
---
+++ django-vue/front/src/components/frontpage/ObjectiveComponent.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+
+<v-row>
+
+
+<v-col>
+<h4>
+{{subtitle}}
+</h4>
+
+<h1>
+{{title}}
+</h1>
+
+<p>
+{{text}}
+</p>
+</v-col>
+
+
+<v-col>
+
+<v-img>
+
+
+
+</v-img>
+
+
+
+</v-col>
+
+</v-row>
+
+
+
+</div>
+
+
+</template>
+
+
+
+<script>
+export default {
+  name: "Objective",
+  data() {
+    return {
+        subtitle:"Objetivo",
+        title:"Construir paz entre todos",
+        text:"Las Narrativas de Paz son relatos construidos colectivamente para definir lo que sí es la paz para los caleños. Estos relatos surgen de talleres que funcionan como espacios de encuentro para la construcción de una definición de paz desde un enfoque territorial. "
+        };
+  }
+};
+</script>
