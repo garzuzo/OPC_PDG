765c3254c590734a8569a757808b9cb057699ab5
garzuzo <garzuxo@gmail.com>
2019-09-11 16:21:58 -0500

    add db configuration

--- django-vue/opc/settings.py
+++ django-vue/opc/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'opc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'opc_db',
+        'USER':os.environ['OPC_USER'],
+        'PASSWORD':os.environ['OPC_PASS'],
+        'HOST': 'localhost',
+        'PORT': '',
+
     }
 }
 
