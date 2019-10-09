626a07ce4ebde2d38002a44816c15cadf04e7491
mishale22 <sami_na11@hotmail.com>
2019-10-09 08:28:39 -0500

    campaigns, frontpage, profile advance

---
+++ django-vue/front/src/components/aboutus/MemberItemComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card class="mx-auto" max-width="400">
+            <v-container>
+              <v-col align="center">
+                <img src="https://picsum.photos/id/11/500/300" class="img" />
+                <h4>Juan Muñoz</h4>
+                <h5>Sociologo</h5>
+              </v-col>
+            </v-container>
+          </v-card>
+
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css?family=Poppins|Roboto&display=swap");
+h4 {
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 36px;
+
+  color: #0c186d;
+}
+
+h5{
+    font-family: "Roboto";
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 36px;
+
+  color: #000000;
+}
+
+.img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin-bottom:10px;
+}
+</style>
