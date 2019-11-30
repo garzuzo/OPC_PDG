332b5bbed1259d16b9bb5cd02b2cc4d4b6cf0cd2
garzuzo <garzuxo@gmail.com>
2019-11-29 22:10:10 -0500

    add process.env.NODE_ENV

--- django-vue/front/src/main.js
+++ django-vue/front/src/main.js
@@ -44,7 +44,7 @@ const files = require.context('./', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 const router = new VueRouter({
-  base: '/opc/',
+  base: process.env.NODE_ENV,
   //mode: 'history',
   routes 
 })
