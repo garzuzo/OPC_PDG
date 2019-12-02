66be9e4ac00ea0e4fcbd5d6fbb2e289ef27589e9
garzuzo <garzuxo@gmail.com>
2019-12-02 00:18:57 -0500

    add baseUrl pi2sis.icesi.edu.co/opcapi

--- django-vue/frontend/src/configaxios.js
+++ django-vue/frontend/src/configaxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://127.0.0.1:8000/opcapi', 
+    baseURL: 'http://pi2sis.icesi.edu.co/opcapi', 
     headers: {
      'X-Requested-With': 'XMLHttpRequest',
      //'Content-Type': 'application/x-www-form-urlencoded', 
